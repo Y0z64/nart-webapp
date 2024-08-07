cdce408e91acf47abf650b11fb75d61a0aa7f001
Y0z64 <yairprogrammer@gmail.com>
2024-08-07 12:54:37 -0800

    Added links and contact info

--- src/components/particles/Footer.tsx
+++ src/components/particles/Footer.tsx
@@ -2,22 +2,22 @@ import pkg from '../../../package.json';
 
 export default function Footer() {
   return (
-    <footer className="min-h-34 flex w-screen items-center justify-between bg-car px-4 py-3 text-[#1e1e1e]">
+    <footer className="min-h-34 bg-car flex w-screen items-center justify-between px-4 py-3 text-[#1e1e1e]">
       <div className="lg:text-md font-geistMono flex h-full flex-col items-start justify-between text-left text-sm">
-        <span className='text-accent-foreground'>Contacto</span>
+        <span className="text-accent-foreground">Contacto</span>
         <a
           className="text-xs text-accent-foreground hover:text-blue-400 lg:text-sm"
-          href=""
+          href="https://chat.whatsapp.com/CmAJU7sIzYJ1ixZaNCQuYF"
           target="_blank"
           rel="noopener noreferrer"
         >
-          www.placeholder.com
+          Grupo de Whatsapp
         </a>
         <a
           className="text-xs text-accent-foreground hover:text-blue-400 lg:text-sm"
-          href="mailto:yairprogrammer@gmail.com?subject=Let's%20Connect!"
+          href="mailto:neurosenseart@gmail.com?subject=Hay que conectar!"
         >
-          placeholder@plaholder.com
+          neurosenseart@gmail.com
         </a>
       </div>
       <div className="flex h-14 flex-col items-end justify-end">
