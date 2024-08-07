f5301649c17f76ab2671e54a1c6e937266291eb3
Y0z64 <yairprogrammer@gmail.com>
2024-08-07 02:42:38 -0600

    Added navbar animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import Nav from "./components/particles/Navbar";
 
 function App() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-start">
-      <Nav />
-    </div>
+    <>
+      <div className="relative flex h-screen w-screen flex-col items-center justify-start">
+        <Nav />
+      </div>
+      <div className="flex h-screen w-screen flex-col items-center justify-start">
+      </div>
+    </>
   );
 }
 
--- src/components/particles/Navbar.tsx
+++ src/components/particles/Navbar.tsx
@@ -1,11 +1,25 @@
-import NavButton from "../atoms/NavButton"
+import NavButton from "../atoms/NavButton";
 import { Button } from "../ui/button";
-import { ModeToggle } from '../mode-toggle.';
-
+import { ModeToggle } from "../mode-toggle.";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
 
 export default function Nav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const { scrollYProgress } = useScroll({  });
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log(latest);
+    console.log(latest < 0.001 ? "true" : "false");
+    setIsScrolled(latest < 0.001);
+  });
+
   return (
-    <div className="mt-1 flex h-14 w-full items-center justify-between px-4">
+    <motion.div
+      layout
+      className="left-0 top-0 mt-1 flex h-14 w-full items-center justify-between px-4"
+      animate={{ y: isScrolled ? 0 : -50}}
+    >
       {/* Left */}
       <div className="mr-2 flex w-fit items-center justify-center space-x-3">
         <NavButton text="PROJECTS" />
@@ -19,6 +33,6 @@ export default function Nav() {
           Register
         </Button>
       </div>
-    </div>
+    </motion.div>
   );
 }
