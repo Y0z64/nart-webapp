609520d268d7a3b2d985fe290a514364a85acb09
Y0z64 <yairprogrammer@gmail.com>
2024-08-07 07:14:48 -0600

    Removed SEX button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Hero from "./components/pages/Hero";
-import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <Hero />
       <div className="flex h-screen w-screen flex-col items-center justify-start">
-        <Button variant={"outline"}>SEX</Button>
       </div>
     </>
   );
